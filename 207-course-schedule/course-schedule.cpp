53c050966ab6087b51e750465f20ecbad12d9f3e
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-07-21 16:30:21 +0530

    Time: 15 ms (69.70%) | Memory: 18.5 MB (18.20%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    
+public:
+    void dfs(int u, unordered_map<int,vector<int>>&adj, vector<int>&vis, stack<int>&st, bool &cycle){
+        vis[u]=1;
+
+        for(auto &i: adj[u]){
+            if(vis[i]==0){
+                dfs(i,adj,vis,st,cycle);
+            }
+            else if(vis[i]==1){
+                cycle=true;
+                return;
+            }
+        }
+        vis[u]=2;
+        st.push(u);
+    }
+    vector<int>topoSort(vector<vector<int>>&edges, int n){
+        unordered_map<int, vector<int>>adj;
+        for(auto &it: edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+        }
+        vector<int>vis(n+1,0);
+        stack<int>st;
+        vector<int>res;
+        bool cycle=false;
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                dfs(i,adj,vis,st,cycle);
+
+                if(cycle){
+                    return {};
+                }
+            }
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res;
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int>topoRow= topoSort(prerequisites,n);
+        if(topoRow.empty()){
+            return false;
+        }
+        return true;
+    }
+};
