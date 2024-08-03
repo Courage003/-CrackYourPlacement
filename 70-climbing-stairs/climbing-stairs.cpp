9762a5e7b8907e12ef2f11f74252af1c7935492b
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-08-03 23:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (16.65%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,6 +4,7 @@ public:
     int dp[46];  // DP array to store results for up to 45 stairs
 
     int solve(int n) {
+        //Top down approach started from 3 then -1 and -2
         if (n < 0)
             return 0;
         if (n == 0)
@@ -18,7 +19,21 @@ public:
     }
 
     int climbStairs(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        /*memset(dp, -1, sizeof(dp));
+        return solve(n);*/
+
+        //Tabulation approach (bottom up)
+        if(n==1 || n==2 || n==3)
+            return n;
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
     }
 };
