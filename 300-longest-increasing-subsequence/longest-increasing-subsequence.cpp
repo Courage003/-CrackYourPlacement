d43c221514aa18a618841846cf3f9527df10ce57
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-08-04 21:49:56 +0530

    Time: 173 ms (43.55%) | Memory: 40.4 MB (40.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -25,9 +25,25 @@ public:
     }
     
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
+        /*int n=nums.size();
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,-1);
+        return solve(nums,0,-1);*/
+
+        int n=nums.size();
+        //Bottom Up Approach
+
+        vector<int>dp(n,1);
+        //state definition
+        int maxLIS=1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i])
+                dp[i]=max(dp[i],dp[j]+1);
+                maxLIS=max(maxLIS,dp[i]);
+            }
+        }
+        return maxLIS;
         
     }
 };
