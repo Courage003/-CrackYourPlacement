f02c5982cff11c22d584c616114f2dc10a783bf0
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-08-13 00:26:13 +0530

    Time: 70 ms (60.04%) | Memory: 58.3 MB (24.40%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,22 +1,45 @@
 class MyHashSet {
 public:
-vector<int>v;
+//vector<int>v;
+vector<list<int>>bucket;
+int M;
+    int getIndex(int key){
+        return key%M;
+    }
     MyHashSet() {
-        v.resize(1000001,false);
+        //v.resize(1000001,false);
+        M=15000;
+        bucket=vector<list<int>>(M,list<int>{});
         
     }
     
     void add(int key) {
-        v[key]=true;
+        //v[key]=true;
+        int idx=getIndex(key);
+        auto itr=find(bucket[idx].begin(),bucket[idx].end(),key);
+
+        if(itr==bucket[idx].end())
+            bucket[idx].push_back(key);
+
+    
         
     }
     
     void remove(int key) {
-        v[key]=false;
+        //v[key]=false;
+        int idx=getIndex(key);
+        auto itr=find(bucket[idx].begin(),bucket[idx].end(),key);
+
+        if(itr!=bucket[idx].end())
+            bucket[idx].erase(itr);
     }
     
     bool contains(int key) {
-        return v[key]==true;
+        //return v[key]==true;
+        int idx=getIndex(key);
+        auto itr=find(bucket[idx].begin(),bucket[idx].end(),key);
+
+        return itr!=bucket[idx].end();
     }
 };
 
