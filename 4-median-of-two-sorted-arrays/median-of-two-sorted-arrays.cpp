a9f28fc9cf0d3471f673a2fd4eee22ea7a151112
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-08-16 01:09:31 +0530

    Time: 20 ms (79.17%) | Memory: 94.8 MB (47.85%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,38 +4,81 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
-        vector<int> temp(m + n);
+        //vector<int> temp(m + n);
         int i = 0;
         int j = 0;
         int k = 0; // Index for temp
+        int size=(m+n);
+        int idx1=(size/2)-1;
+        int elem1=-1;
+        int idx2=(size/2);
+        int elem2=-1;
 
         // Merge the two arrays
         while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
+            /*if (nums1[i] < nums2[j]) {
                 temp[k++] = nums1[i++];
             } else {
                 temp[k++] = nums2[j++];
+            }*/
+
+            if(nums1[i]<nums2[j]){
+                if(k==idx1){
+                    elem1=nums1[i];
+                }
+                if(k==idx2){
+                    elem2=nums1[i];
+                }
+                i++;
             }
+            else{
+                if(k==idx1){
+                    elem1=nums2[j];
+                }
+                if(k==idx2){
+                    elem2=nums2[j];
+                }
+                j++;
+            }
+            k++;
         }
 
         // If there are remaining elements in nums1
         while (i < m) {
-            temp[k++] = nums1[i++];
+            //temp[k++] = nums1[i++];
+            if(k==idx1){
+                    elem1=nums1[i];
+                }
+            if(k==idx2){
+                    elem2=nums1[i];
+                }
+                i++;
+                k++;
         }
 
         // If there are remaining elements in nums2
         while (j < n) {
-            temp[k++] = nums2[j++];
+            //temp[k++] = nums2[j++];
+            if(k==idx1){
+                    elem1=nums2[j];
+                }
+            if(k==idx2){
+                    elem2=nums2[j];
+                }
+                j++;
+                k++;
         }
 
-        int size = temp.size();
+        //int size = temp.size();
 
         // If the merged array size is odd, return the middle element
         if (size % 2 == 1) {
-            return temp[size / 2];
+            //return temp[size / 2];
+            return elem2;
         }
 
         // If the merged array size is even, return the average of the two middle elements
-        return (temp[size / 2] + temp[(size / 2) - 1]) / 2.0;
+        //return (temp[size / 2] + temp[(size / 2) - 1]) / 2.0;
+        return (elem1+elem2)/2.0;
     }
 };
