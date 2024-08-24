ac5b1a011fc33d598dad0b55bd47f1b9624ec2c4
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2024-08-24 17:55:20 +0530

    Time: 33 ms (19.79%) | Memory: 94.6 MB (82.43%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,7 +4,7 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
-        //vector<int> temp(m + n);
+        /*//vector<int> temp(m + n);
         int i = 0;
         int j = 0;
         int k = 0; // Index for temp
@@ -20,7 +20,7 @@ public:
                 temp[k++] = nums1[i++];
             } else {
                 temp[k++] = nums2[j++];
-            }*/
+            }
 
             if(nums1[i]<nums2[j]){
                 if(k==idx1){
@@ -79,6 +79,44 @@ public:
 
         // If the merged array size is even, return the average of the two middle elements
         //return (temp[size / 2] + temp[(size / 2) - 1]) / 2.0;
-        return (elem1+elem2)/2.0;
+        return (elem1+elem2)/2.0;*/
+
+        //Binary Search approach
+
+
+        if(m>n){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+
+        int l=0;
+        int r=m;
+
+        while(l<=r){
+            int Px=l+(r-l)/2;
+            int Py=(m+n+1)/2-Px;
+            //left half
+            int x1=(Px==0)? INT_MIN: nums1[Px-1];
+            int x2=(Py==0)? INT_MIN: nums2[Py-1];
+            //right half
+            int x3=(Px==m)? INT_MAX: nums1[Px];
+            int x4=(Py==n)? INT_MAX: nums2[Py];
+
+            if(x1<=x4 && x2<=x3){
+                if((m+n)%2==0){
+                    return (max(x1,x2)+min(x3,x4))/2.0;
+                }
+                else{
+                return max(x1,x2);
+                }
+            }
+
+            if(x1>x4){
+                r=Px-1;
+            }
+            else{
+                l=Px+1;
+            }
+        }
+        return -1;
     }
 };
