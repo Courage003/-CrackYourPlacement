6d5e011078eb66de21c7fd6358a7ae8e66d91216
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2025-04-16 00:36:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -5,47 +5,52 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
 
-   bool allZero(vector<int>& counter) {
-        for (int &i : counter) {
-            if (i != 0) return false;  // Check all elements
+// User function template for C++
+class Solution {
+  public:
+    bool allZero(vector<int>&counter){
+        for(auto &i: counter){
+            if(i!=0){
+                return false;
+            }
+            
         }
         return true;
     }
-	int search(string pat, string txt) {
-	    // code here
-	    int n=txt.size();
-	    vector<int>counter(26,0);
-	    
-	    //count frequency of each chars
-	    for(int i=0;i<pat.size();i++){
-	        char ch=pat[i];
-	        counter[ch-'a']++;
-	    }
-	    
-	    int i=0,j=0;
-	    int res=0;
-	    while(j<n){
-	        int k=pat.size();
-	        counter[txt[j]-'a']--;
-	        
-	        if(j-i+1==k){
-	            if(allZero(counter)){
-	                res++;
-	            }
-	            
-	            counter[txt[i]-'a']++;
-	            i++;
-	        }
-	        j++;
-	    }
-	    return res;
-	}
-
+    int search(string &pat, string &txt) {
+        // code here
+        int n=txt.size();
+        
+        //keeping frequency of words present in pat word
+        vector<int>counter(26,0);
+        for(int i=0;i<pat.size();i++){
+            char ch=pat[i];
+            counter[ch-'a']++;
+        }
+        
+        //SLiding window approach
+        int i=0,j=0;
+        
+        int res=0;
+        while(j<n){
+            counter[txt[j]-'a']--;
+            
+            if(j-i+1==pat.size()){
+                if(allZero(counter)){
+                    res++;
+                }
+                
+                counter[txt[i]-'a']++;
+                i++;
+            }
+            j++;
+            
+        }
+        return res;
+    }
 };
+
 
 //{ Driver Code Starts.
 
@@ -58,6 +63,9 @@ int main() {
         Solution ob;
         auto ans = ob.search(pat, txt);
         cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
